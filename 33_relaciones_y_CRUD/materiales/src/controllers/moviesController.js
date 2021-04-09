ab612556791d60a6450bd16c1831dcba8710b032
Santiago Argilla <sargilla@gmail.com>
2021-04-09 18:59:08 -0300

    inicio clase 33

---
+++ 33_relaciones_y_CRUD/materiales/src/controllers/moviesController.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+
+
+//Aqui tienen una forma de llamar a cada uno de los modelos
+// const {Movies,Genres,Actor} = require('../database/models');
+
+//Aquí tienen otra forma de llamar a los modelos creados
+const Movies = db.Movie;
+const Genres = db.Genre;
+const Actors = db.Actor;
+
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    },
+    //Aqui dispongo las rutas para trabajar con el CRUD
+    add: function (req, res) {
+        
+    },
+    create: function (req,res) {
+
+    },
+    edit: function(req,res) {
+
+    },
+    update: function (req,res) {
+
+    },
+    delete: function (req,res) {
+
+    },
+    destroy: function (req,res) {
+
+    }
+}
+
+module.exports = moviesController;
---
+++ 33_relaciones_y_CRUD/repaso.md
@@ -0,0 +1,7 @@
+# Relaciones y CRUD
+
+## Objetivos
+- Practicar el proceso de creación, actualización y borrado de registros. 
+- Reforzar la razón y utilidad de la capa del modelo. 
+- Comprender cómo leer y escribir información cuando estos datos involucran más de una tabla de la base de datos.
+- Ejercitar sobre la escritura y relaciones en la base de datos. 
